c52d0491290f6d6ffbb4dc14fa73deb7f3e2ae9e
Kenny Hoang <khoang15@horizon.csueastbay.edu>
2020-03-25 18:08:13 -0700

    Adding classes for Pokemon,PokemonCollection, Move, MoveCollection plus the two data text files.

---
+++ Move.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Move {
+	
+	// Pokemon attribute move-set, each Pokemon can hold 4 moves at most. 
+	private String moveName; 
+	private String moveType;
+	private String moveTypeCategory; 
+	private int moveDmg; 
+	
+	public Move(String moveName, String moveType, String moveTypeCategory, int moveDmg)
+	{
+		this.moveName = moveName;
+		this.moveType = moveType;
+		this.moveTypeCategory = moveTypeCategory;
+		this.moveDmg = moveDmg; 
+	}
+		
+	// setters for Pokemon attribute move-set, each Pokemon can hold 4 moves at most. 
+	public void setMoveName(String thisMoveName) { this.moveName = thisMoveName; }
+	public void setMoveType(String thisMoveType) { this.moveType = thisMoveType; }
+	public void setMoveCategory(String thisMoveTypeCat) { this.moveTypeCategory = thisMoveTypeCat; }
+	public void setMoveDmg(int thisMoveDmg) { this.moveDmg = thisMoveDmg; }
+	
+	// getters for Pokemon attribute move-set, each Pokemon can hold 4 moves at most. 
+	public String getMoveName() { return moveName; }
+	public String getMoveType() { return moveType; }
+	public String getMoveTypeCat() { return moveTypeCategory; }
+	public int getMoveDamage() { return moveDmg; }
+	
+} // end of Move class
