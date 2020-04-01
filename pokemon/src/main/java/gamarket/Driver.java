df4ac4867a5b3999a014822c213296db7a428560
David Serrano <davidserrano@Davids-MBP-2.attlocal.net>
2020-03-31 18:40:22 -0700

    implemented driver in order to test with out maven/jUnit due to filepath issues

--- pokemon/src/main/java/gamarket/Driver.java
+++ pokemon/src/main/java/gamarket/Driver.java
@@ -5,8 +5,19 @@ import java.util.*;
 public class Driver {
 
     public static void main(String args[]){
-        StartMenuGUI startMenuGUI = new StartMenuGUI();
-        System.out.println("sis ran");
+
+        Player player = new Player(true, "joe", "password");
+        player.setEmail("joe@email.com");
+        player.saveData();
+
+
+        player = new Player(false, "david", "password");
+
+        int badgeAmt = player.getBadges();
+        badgeAmt--;
+        player.setBadges(badgeAmt);
+
+        player.saveData();
 
     }
 }
