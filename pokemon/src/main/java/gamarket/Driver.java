8c30d2607e6e8b2706216dfd25fb8a8587b21174
David Serrano <davidserrano@Davids-MBP-2.attlocal.net>
2020-04-09 21:29:07 -0700

    small test fix

--- pokemon/src/main/java/gamarket/Driver.java
+++ pokemon/src/main/java/gamarket/Driver.java
@@ -1,6 +1,6 @@
 package gamarket;
 
-import java.util.*;
+
 
 public class Driver {
 
@@ -9,13 +9,21 @@ public class Driver {
         Player player = new Player(true, "joe", "password");
         player.setEmail("joe@email.com");
         player.saveData();
-
+        System.out.println(player.toString());
 
         player = new Player(false, "david", "password");
+        System.out.println(player.toString());
 
         int badgeAmt = player.getBadges();
         badgeAmt--;
         player.setBadges(badgeAmt);
+        System.out.println(player.toString());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         player.saveData();
 
